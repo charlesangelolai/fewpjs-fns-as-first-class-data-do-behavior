19a4f06aa2b7ac142de05e184cf28f8f24915c2c
Charles Lai <charlesangeloplai@gmail.com>
2021-02-19 00:36:39 -0500

    commit tree 9358fe1c830d087c41327a07b5998780c4e6f316

--- index.js
+++ index.js
@@ -3,11 +3,20 @@
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById("time").value;
+  displayMessage(greet(timeString));
 }
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(t) {
+  t = parseInt(t, 10);
+  if (t < 12) return "Good Morning";
+  if (t > 17) return "Good Evening";
+  else return "Good Afternoon";
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(msg) {
+  document.getElementById("greeting").innerText = msg;
+}
